1fba3c652f961dacb6d8877291747019b9353496
Wagner Macedo <wagnerluis1982@gmail.com>
2012-01-29 17:31:11 -0300

    Mudança para download_pagina() lidar com encodings

--- src/gval/__init__.py
+++ src/gval/__init__.py
@@ -1,9 +1,14 @@
 import cookielib
 import urllib2
 
-def download_pagina(url):
+def download_pagina(url, charset=None):
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     page = opener.open(url)
 
-    return page.read()
+    charset = charset or page.headers.getparam('charset')
+
+    if charset:
+        return unicode(page.read(), charset)
+    else:
+        return page.read()
