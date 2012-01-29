9846715ea5d93fc5770eb6b65677c7ff9c4843d9
Wagner Macedo <wagnerluis1982@gmail.com>
2012-01-29 20:40:20 -0300

    Forma mais eficiente de lidar com encodings

--- src/gval/__init__.py
+++ src/gval/__init__.py
@@ -5,10 +5,11 @@ def download_pagina(url, charset=None):
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     page = opener.open(url)
+    page_data = page.read()
 
     charset = charset or page.headers.getparam('charset')
 
-    if charset:
-        return unicode(page.read(), charset)
-    else:
-        return page.read()
+    try:
+        return unicode(page_data, charset)
+    except (UnicodeDecodeError, TypeError, LookupError):
+        return page_data
