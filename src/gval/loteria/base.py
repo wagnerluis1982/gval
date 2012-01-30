f511931b2f2c4ba6e0522cf2c00972418da43ff9
Wagner Macedo <wagnerluis1982@gmail.com>
2012-01-30 20:46:35 -0300

    Refatoração na classe Loteria
    Preparando para a adição de novas subclasses de Loteria
    
    Mudanças ocorridas:
    - URL_CONSULTA passa de constante para o atributo protegido _url_loteria
    - _url_consulta() ganha mais um _, torna assim, um método protegido
    - consultar() passa a ser parte de Loteria
    - Em consequência, _extrair_resultado() passa a ser método "abstrato".
    - Por fim, uma muito pequena organização do código em _extrair_resultado()
      na classe Lotofacil

--- src/gval/loteria/base.py
+++ src/gval/loteria/base.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
+import gval
 
 # abstract class
 class Loteria(object):
-    _loteria = None
-
-    def __init__(self, concurso, url=None):
-        self.url = url or self._url_consulta(self._loteria, concurso)
-        self.html = None
-
     URL_BASE = "http://www1.caixa.gov.br/loterias/loterias/"
-    URL_CONSULTA = ("{loteria}/{loteria}_pesquisa_new.asp?submeteu=sim&opcao="
+
+    _loteria = None
+    _url_loteria = ("{loteria}/{loteria}_pesquisa_new.asp?submeteu=sim&opcao="
                     "concurso&txtConcurso={concurso}")
 
-    # abstract method
+    def __init__(self, concurso, url=None):
+        self.url = url or self.__url_consulta(self._loteria, concurso)
+        self.html = None
+
     def consultar(self):
+        self.html = self.html or gval.download_pagina(self.url)
+
+        return self._extrair_resultado(self.html)
+
+    # abstract method
+    def _extrair_resultado(self, html):
         raise NotImplementedError("Você deve sobrescrever esse método")
 
-    def _url_consulta(self, loteria, concurso):
+    def __url_consulta(self, loteria, concurso):
         dados_aposta = {'loteria':loteria, 'concurso':concurso}
-        return self.URL_BASE + self.URL_CONSULTA.format(**dados_aposta)
+        return self.URL_BASE + self._url_loteria.format(**dados_aposta)
--- src/gval/loteria/lotofacil.py
+++ src/gval/loteria/lotofacil.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
-from gval import download_pagina
 from gval.loteria import Loteria
 from gval.loteria.parser import LoteriaParser
 
 class Lotofacil(Loteria):
     _loteria = 'lotofacil'
 
-    def consultar(self):
-        self.html = self.html or download_pagina(self.url)
-
-        return self._extrair_resultado(self.html)
-
     def _extrair_resultado(self, html):
         parser = LoteriaParser()
         parser.feed(html)
+        resultado = parser.dados.split('|')
 
         POSICAO = dict(
             concurso=0,
             numeros=range(3, 18)
         )
-
-        resultado = parser.dados.split('|')
 
         return {'concurso': int(resultado[POSICAO['concurso']]),
                 'numeros': [int(resultado[n]) for n in POSICAO['numeros']]}
