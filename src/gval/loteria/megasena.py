8e682b799afca44f773ec46a637333d293c4e1ec
Wagner Macedo <wagnerluis1982@gmail.com>
2012-07-24 15:11:44 -0300

    Implementação da conferência da Mega Sena
    E conclusão da feature "Apostador confere aposta".

--- src/gval/loteria/megasena.py
+++ src/gval/loteria/megasena.py
@@ -4,5 +4,7 @@ from gval.loteria import Loteria, Posicao
 class MegaSena(Loteria):
     posicao = Posicao(
         numeros = xrange(28, 34),
-        premios = {},
+        premios = {6: (10, 11),
+                   5: (12, 13),
+                   4: (14, 15)},
     )
